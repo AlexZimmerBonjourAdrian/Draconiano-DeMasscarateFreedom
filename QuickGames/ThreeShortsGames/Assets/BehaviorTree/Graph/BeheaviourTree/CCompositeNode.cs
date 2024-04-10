2982107e6ab77f00509d2e3881b4ebf1e70ed9b4
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2024-04-10 02:17:27 -0300

    Respaldo AI beheavuir Tree new Implementation

--- QuickGames/ThreeShortsGames/Assets/BehaviorTree/Graph/BeheaviourTree/CCompositeNode.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class CCompositeNode : CNode
-{
-    [HideInInspector] public CNode child;
-    public List<CNode> children = new List<CNode>();
-}
