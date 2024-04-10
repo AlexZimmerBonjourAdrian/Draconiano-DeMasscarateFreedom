4ceead85f4c6d937c33175c871b8302da4f7475b
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2024-04-10 01:54:57 -0300

    Respaldo

---
+++ QuickGames/ThreeShortsGames/Assets/ScriprControlGameGeneric/LimitationFPS.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitationFPS : MonoBehaviour
+{
+    //private int limitateFPS = 60;
+  //  public DataFPSConfig DataFPS;
+    private void Start()
+    {
+        //Application.targetFrameRate = DataFPS.ActualFPS;
+    }
+}
